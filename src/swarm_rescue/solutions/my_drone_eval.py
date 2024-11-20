2bf918b5a49e87b88e1222423fc58578c91dcad5
makessa.kone <mak@MacBook-Air-de-Makessa.local>
2024-11-20 22:53:24 +0100

    Ajout explication test

--- src/swarm_rescue/solutions/my_drone_eval.py
+++ src/swarm_rescue/solutions/my_drone_eval.py
@@ -2,4 +2,7 @@ from solutions.my_drone_random import MyDroneRandom
 from solutions.my_drone_lidar_communication import MyDroneLidarCommunication
 
 class MyDroneEval(MyDroneLidarCommunication):
+    """
+    Test ##
+    """
     pass
