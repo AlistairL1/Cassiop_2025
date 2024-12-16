9ee14645773662d70bba16e9877d712e8d89b8bd
makessa.kone <mak@MacBook-Air-de-Makessa.local>
2024-12-16 01:14:14 +0100

    J'ai importé des trucs, j'ai mis les lignes de code de def à super qui sont présentes dans tous les codes même si j'ai pas encore capté ce que c'est , et j'ai mis les 4 self qui permettent un peu de contrôler la direction mais là c'est fait de manière aléatoire donc faut le modifier, j'ai mis en commentaire ce que chaque truc fait

--- src/swarm_rescue/solutions/my_drone_eval.py
+++ src/swarm_rescue/solutions/my_drone_eval.py
@@ -1,5 +1,28 @@
+from typing import Optional
 from solutions.my_drone_random import MyDroneRandom
-from solutions.my_drone_lidar_communication import MyDroneLidarCommunication
+from solutions.my_drone_lidar_communication import MyDroneLidarCommunication, MiscData
+import random
+from random import uniform
+import math
 
 class MyDroneEval(MyDroneLidarCommunication):
-    pass
+    def __init__(self,
+                 identifier: Optional[int] = None,
+                 misc_data: Optional[MiscData] = None,
+                 **kwargs):
+        super().__init__(identifier=identifier,
+                         misc_data=misc_data,
+                         display_lidar_graph=False,
+                         **kwargs)
+
+        self.counterStraight = 0 #compteur utilisé pour suivre le nombre d'itérations òù le drone se déplace en ligne droite.
+        self.angleStopTurning = random.uniform(-math.pi, math.pi) #définit un angle aléatoire entre −π−π et ππ (en radians), qui représente la direction vers laquelle le drone doit arrêter de tourner
+        self.distStopStraight = random.uniform(10, 50) #Définit une distance aléatoire entre 10 et 50  que le drone doit parcourir en ligne droite avant de changer de comportement
+        self.isTurning = False #Un booléen qui indique si le drone est en train de tourner ou non
+
+
+
+
+
+
+
