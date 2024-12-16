20cf3e9049a4a47df0b0c9ebf0ac9e0c0c501768
makessa.kone <mak@MacBook-Air-de-Makessa.local>
2024-12-16 01:17:36 +0100

    J'ai importé des trucs, j'ai mis les lignes de code de def à super qui sont présentes dans tous les codes même si j'ai pas encore capté ce que c'est , et j'ai mis les 4 self qui permettent un peu de contrôler la direction mais là c'est fait de manière aléatoire donc faut le modifier, j'ai mis en commentaire ce que chaque truc fait

--- src/swarm_rescue/solutions/my_drone_random.py
+++ src/swarm_rescue/solutions/my_drone_random.py
@@ -20,10 +20,10 @@ class MyDroneRandom(DroneAbstract):
                          misc_data=misc_data,
                          display_lidar_graph=False,
                          **kwargs)
-        self.counterStraight = 0
-        self.angleStopTurning = random.uniform(-math.pi, math.pi)
-        self.distStopStraight = random.uniform(10, 50)
-        self.isTurning = False
+        self.counterStraight = 0 #compteur utilisé pour suivre le nombre d'itérations òù le drone se déplace en ligne droite.
+        self.angleStopTurning = random.uniform(-math.pi, math.pi) #définit un angle aléatoire entre −π−π et ππ (en radians), qui représente la direction vers laquelle le drone doit arrêter de tourner
+        self.distStopStraight = random.uniform(10, 50) #Définit une distance aléatoire entre 10 et 50  que le drone doit parcourir en ligne droite avant de changer de comportement
+        self.isTurning = False #Un booléen qui indique si le drone est en train de tourner ou non
 
     def define_message_for_all(self):
         """
