1b35f24ce0c32ad2b954cb16aba6a11785ed4f30
alistair <alistair.lucet@gmail.com>
2024-12-21 14:13:07 -0500

    Nettoyage du repo

--- src/swarm_rescue/solutions/my_drone_eval.py
+++ src/swarm_rescue/solutions/my_drone_eval.py
@@ -8,14 +8,6 @@ import math
 class MyDroneEval(MyDroneLidarCommunication):
     pass
 
-#on a notre position , une trajectoire de reference que le drone doit suivre on veut aller de a vers b => suivre une trajectoire
-#on a deux points a et b et il faut le rejoindre , si trajectoire constante (voir lien chat) - graphe ligne pointillée qui est la référence , en utilisant le régulateur - refefrence se deplace vers le point B - si on deplace la reference on ralenti et si on ne l'atteint pas on accelère
-#le truc le plus compliqué : comment choisir A et B de façon à ce que ça suivre notre trajectoire
-#se baser sur des techniques qui sont basées sur des slams
-#plutôt position de référence ou vitesse de référence ? si on connaît la dérivée, c'est plus précis
-
-
-
 
 
 
