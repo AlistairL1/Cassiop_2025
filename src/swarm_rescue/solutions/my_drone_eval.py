f9458fb8acb72947042cbd71df6e6a390d66e515
alistair <alistair.lucet@gmail.com>
2025-03-06 17:39:31 +0100

    MAJ

--- src/swarm_rescue/solutions/my_drone_eval.py
+++ src/swarm_rescue/solutions/my_drone_eval.py
@@ -2,7 +2,7 @@ from spg_overlay.entities.drone_abstract import DroneAbstract
 import numpy as np
 from typing import Optional
 from spg_overlay.utils.misc_data import MiscData
-from spg_overlay.utils.utils import clamp
+from spg_overlay.utils.utils import clamp, normalize_angle, rad2deg
 
 class MyDroneEval(DroneAbstract):
     def __init__(self,
@@ -39,16 +39,16 @@ class MyDroneEval(DroneAbstract):
 
         # Contrôle forward (x)
         Kp_forward = 0.75
-        a_forward = max(0, 4/np.pi * angle_diff_x)
+        a_forward = max(0, 8/np.pi * angle_diff_x*distance_to_target)
         command["forward"] = clamp(Kp_forward * a_forward * distance_to_target, -1.0, 1.0)
 
         # Contrôle latéral (y)
-        Kp_lateral = 0
-        a_latetal = max(0, 4/np.pi * angle_diff_x)
-        command["lateral"] = clamp(Kp_lateral * a_latetal * distance_to_target, -1.0, 1.0)
+        # Kp_lateral = 0.7
+        # a_latetal = max(0, 4/np.pi * (angle_diff_x))
+        # command["lateral"] = clamp(Kp_lateral * a_latetal * distance_to_target, -1.0, 1.0)
 
         # Contrôle rotation (θ)
-        Kp_rotation = .5
-        command["rotation"] = clamp(Kp_rotation * angle_diff_x * distance_to_target, -1.0, 1.0)
+        Kp_rotation = .25
+        command["rotation"] = clamp(angle_diff_x*Kp_rotation, -1.0, 1.0)
 
         return command
